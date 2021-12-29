62c47e6eb704de869af17f6f65ffec01654e383c
kiangkiangkiang <44896160+kiangkiangkiang@users.noreply.github.com>
2021-12-29 12:34:54 +0800

    Add files via upload

---
+++ crawl/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Stocksidname, Stockstypelink
+# Register your models here.
+admin.site.register(Stocksidname)
+admin.site.register(Stockstypelink)
